72a6549b798d8b56bec46d94b548b15d19b7d5ee
parunkov <parunkov@gmail.com>
2023-09-12 08:39:43 +0500

    currency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.scss';
-import { FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+import { FormGroup, FormControlLabel, Checkbox, Card, CardContent, Typography, ToggleButtonGroup, ToggleButton } from '@mui/material';
 
 function App() {
   const usdCourse = 95;
@@ -8,6 +8,7 @@ function App() {
   const allTransfers = [0, 1, 2, 3];
 
   const [transfers, setTransfers] = useState([0, 1, 2]);
+  const [currency, setCurrency] = useState('₽');
 
   const onTransferChange = (item: number) => {
     if (transfers.includes(item)) {
@@ -23,21 +24,50 @@ function App() {
       setTransfers([]);
     }
   }
+  const onCurrencyChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newCurrency: string,
+  ) => {
+    setCurrency(newCurrency);
+    console.log(currency);
+    
+  }
 
   return (
     <div className="app">
       <div className="container">
         <aside className="control">
-          <FormGroup>
-            <FormControlLabel control={<Checkbox 
-              onChange={onAllTransfersChange} 
-              checked={transfers.length > 3}
-            />} label="Все" />
-            {allTransfers.map((item) => <FormControlLabel key={item} control={<Checkbox 
-              onChange={() => onTransferChange(item)}
-              checked={transfers.includes(item) ? true : false}
-            />} label={`${item === 0 ? '' : item} ${item === 0 ? 'Без пересадок' : item === 1 ? 'пересадка' : 'пересадки'}`} />)}
-          </FormGroup>
+          <Card>
+            <CardContent>
+            <Typography component="div">
+                ВАЛЮТА
+              </Typography>
+              <ToggleButtonGroup
+                color="primary"
+                value={currency}
+                exclusive
+                onChange={onCurrencyChange}
+                aria-label="Platform"
+              >
+                <ToggleButton value="₽">RUB</ToggleButton>
+                <ToggleButton value="$">USD</ToggleButton>
+                <ToggleButton value="€">EUR</ToggleButton>
+              </ToggleButtonGroup>
+              <Typography component="div">
+                КОЛИЧЕСТВО ПЕРЕСАДОК
+              </Typography>
+              <FormGroup>
+                <FormControlLabel control={<Checkbox
+                  onChange={onAllTransfersChange}
+                  checked={transfers.length > 3}
+                />} label="Все" />
+                {allTransfers.map((item) => <FormControlLabel key={item} control={<Checkbox
+                  onChange={() => onTransferChange(item)}
+                  checked={transfers.includes(item) ? true : false}
+                />} label={`${item === 0 ? '' : item} ${item === 0 ? 'Без пересадок' : item === 1 ? 'пересадка' : 'пересадки'}`} />)}
+              </FormGroup>
+            </CardContent>
+          </Card>
         </aside>
       </div>
     </div>
